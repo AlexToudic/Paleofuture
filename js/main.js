52e5c930c17d92b383d4d5744f54cd1df983e622
Alexandre Toudic <alexandre.toudic@gmail.com>
2012-11-18 14:28:34 +0100

    Second commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(function() {
 	//$('#grid').css({'height': window.innerHeight-$('header').css('height').substr(0, $('header').css('height').length-2)+'px'});
 
 	$('#travel').on('click', function(){
-		$('#home').animate({'margin-top': -window.innerHeight+'px'}, 1000);
+		$('#home').animate({'margin-top': -window.innerHeight+'px'}, 500);
+	});
+
+	$('#main-articles').children().css({'margin-top': window.innerHeight+'px'});
+
+
+	var i = 0;
+	$('.grid').on('click', function(){
+		$($('#main-articles').children()[i]).animate({'margin-top': '10px'}, 500);
+		++i;
 	});
 });
