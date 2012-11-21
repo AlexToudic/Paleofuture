29d873814accd5140b8d1f309ff700207f6e4ab2
Alexandre Toudic <alexandre.toudic@gmail.com>
2012-11-21 11:30:36 +0100

    Seventh commit

--- js/main.js
+++ js/main.js
@@ -19,6 +19,9 @@ $(function() {
 		$('#content').removeClass('flip');
 	});
 
+	/*----------------------------------------------------
+				  2- BACKBONE DECLARATIONS
+	----------------------------------------------------*/
 
 	var Article = Backbone.Model.extend();
 
@@ -26,5 +29,50 @@ $(function() {
 		model: Article
 	});
 
+	var ArticlesBlock = Backbone.Model.extend({
+		articles: new Articles
+	});
 
+	var ArticlesBlocks = Backbone.Collection.extend({
+		model: ArticlesBlock
+	});
+
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            "*actions": "defaultRoute" // matches http://example.com/#anything-here
+        }
+    });
+    // Initiate the router
+    var app_router = new AppRouter;
+
+    app_router.on('route:defaultRoute', function(actions) {
+    })
+
+	/*----------------------------------------------------
+				  		3- APPLICATION
+	----------------------------------------------------*/
+	allArticles = new ArticlesBlock();
+
+	allArticles.fetch({ url: "init.json" }).complete(function() {
+    	console.log(allArticles);
+	});
+
+	var amount = 75;
+	var tempDelta = 0;
+	var scroll = 0;
+	$('#space').mousewheel(function(event, delta, deltaX, deltaY) {
+		++scroll;
+		if(scroll%5===0)
+		{
+			if(tempDelta < delta)
+				tempDelta += 1;
+			if(tempDelta > delta)
+				tempDelta -= 1;
+
+			amount += tempDelta;
+		    $('#grid').css({'-webkit-filter': 'custom(url(css/shaders/detached_tiles.vs) mix(url(css/shaders/detached_tiles.fs) normal source-atop), 400 1 border-box detached, amount '+parseInt(amount, 10)+', t 10.0)'});
+		}
+	});
+
+    Backbone.history.start();
 });
