2313206c882ce53ad5a975a2c5843fd360da1481
Alexandre Toudic <alexandre.toudic@gmail.com>
2012-11-22 14:05:31 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,6 @@
 $(function() {
 	/*----------------------------------------------------
-				  1- INTERFACE ADAPTATION
-	----------------------------------------------------*/
-
-	$('#travel').on('click', function(){
-		$('#home').animate({'margin-top': -window.innerHeight+'px'}, 500);
-	});
-
-	$('#layer2').css({'margin-top': -window.innerHeight+50+'px'});
-	$('#interactive').css({'margin-top': -window.innerHeight+'px'});
-
-	$('#content').on('click', function(){
-		$('#article-details').addClass('flip');
-		$('#content').addClass('flip');
-	});
-
-	$('#article-details').on('click', function(){
-		$('#article-details').removeClass('flip');
-		$('#content').removeClass('flip');
-	});
-
-	/*----------------------------------------------------
-				  2- BACKBONE DECLARATIONS
+				  1- BACKBONE DECLARATIONS
 	----------------------------------------------------*/
 
 	var Article = Backbone.Model.extend();
@@ -100,6 +79,47 @@ $(function() {
     })
 
 	/*----------------------------------------------------
+				  2- INTERFACE ADAPTATION
+	----------------------------------------------------*/
+
+	$('#travel').on('click', function(){
+		$('#home').animate({'margin-top': -window.innerHeight+'px'}, 500);
+	});
+
+	$('#layer2').css({'margin-top': -window.innerHeight+50+'px'});
+	$('#interactive').css({'margin-top': -window.innerHeight+'px'});
+
+	$('#content').on('click', function(){
+		$('#article-details').addClass('flip');
+		$('#content').addClass('flip');
+	});
+
+	$('#article-details').on('click', function(){
+		$('#article-details').removeClass('flip');
+		$('#content').removeClass('flip');
+	});
+
+	$('#cursor').draggable({axis: 'x', containment:'#timeline', stop: function(event, ui){
+		var nearestMarker = $($('#timeline .timemarker').get(0));
+		var handleCenter = $('#cursor').width()/2;
+
+		$('#timeline .timemarker').each(function(index, timemarker){
+			var offset = nearestMarker.offset().left ; 
+            var dist = Math.abs(offset - ($('#cursor').offset().left + handleCenter));
+
+            var markerOffset = $(timemarker).offset().left;
+            var markerDist = Math.abs(markerOffset - ($('#cursor').offset().left + handleCenter)); 
+
+
+     		if(markerDist < dist){
+     			nearestMarker = $(timemarker);
+     		}
+		});
+
+		$('#cursor').animate({'left': nearestMarker.offset().left - handleCenter}, 200)
+	}});
+
+	/*----------------------------------------------------
 				  		3- APPLICATION
 	----------------------------------------------------*/
 	allArticles = new ArticlesBlock();
@@ -110,8 +130,6 @@ $(function() {
 
 	var amount = 75;
 	$('#layer2').css({'-webkit-filter': 'custom(url(css/shaders/slices.vs) mix(url(css/shaders/slices.fs) normal source-atop), 100 1 border-box detached, amount '+amount+', t 10.0)'});
-	//$('#grid').css({'-webkit-transform': 'scale(1)'});
-
 	$('#layer1').css({'-webkit-filter': 'custom(url(css/shaders/slices.vs) mix(url(css/shaders/slices.fs) normal source-atop), 100 1 border-box detached, amount '+(amount+2893)+', t 10.0)'});
 
 
@@ -119,6 +137,7 @@ $(function() {
 	var scroll2 = Math.pow(200*(amount+2893), (1/3));
 
 	$('#space').mousewheel(function(event, delta, deltaX, deltaY) {
+		console.log(delta);
 		if(delta > 0)
 		{
 			scroll1 += 1.0;
@@ -135,7 +154,6 @@ $(function() {
 
 	    d2Amount = parseInt((scroll2*scroll2*scroll2)/200, 10);
 	    $('#layer1').css({'-webkit-filter': 'custom(url(css/shaders/slices.vs) mix(url(css/shaders/slices.fs) normal source-atop), 100 1 border-box detached, amount '+d2Amount+', t 10.0)'});
-		//$('#grid').css({'-webkit-transform': 'scale('+0.2*dAmount+')'});
 	});
 
 	var particulesInteraction = function(){
